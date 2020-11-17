6b5585084f3206e51206dd241dd4d7a9213a7355
obito <64253722+obito@users.noreply.github.com>
2020-11-17 14:53:01 +0100

    adding cookie jar

--- client.go
+++ client.go
@@ -1,24 +1,39 @@
 package cclient
 
 import (
-	"golang.org/x/net/proxy"
 	"net/http"
+	"net/http/cookiejar"
+
+	"golang.org/x/net/proxy"
 
 	utls "github.com/refraction-networking/utls"
 )
 
-func NewClient(clientHello utls.ClientHelloID, proxyUrl ...string) (http.Client, error) {
+func NewClient(clientHello utls.ClientHelloID, enableJar bool, proxyUrl ...string) (http.Client, error) {
+	var jar http.CookieJar
+	var err error
+
+	if enableJar {
+		jar, err = cookiejar.New(nil)
+		if err != nil {
+			return http.Client{}, err
+		}
+	}
+
 	if len(proxyUrl) > 0 && len(proxyUrl) > 0 {
 		dialer, err := newConnectDialer(proxyUrl[0])
 		if err != nil {
 			return http.Client{}, err
 		}
+
 		return http.Client{
 			Transport: newRoundTripper(clientHello, dialer),
+			Jar:       jar,
 		}, nil
 	} else {
 		return http.Client{
 			Transport: newRoundTripper(clientHello, proxy.Direct),
+			Jar:       jar,
 		}, nil
 	}
 }

5165c51dd4634cc24da68d7c37aa00f1761bfa59
obito <64253722+obito@users.noreply.github.com>
2020-11-17 14:54:13 +0100

    update test

--- client_test.go
+++ client_test.go
@@ -25,7 +25,7 @@ func readAndClose(r io.ReadCloser) ([]byte, error) {
 
 const Chrome83Hash = "b32309a26951912be7dba376398abc3b"
 
-var client, _ = NewClient(tls.HelloChrome_83) // cannot throw an error because there is no proxy
+var client, _ = NewClient(tls.HelloChrome_83, false) // cannot throw an error because there is no proxy
 
 func TestCClient_JA3(t *testing.T) {
 	resp, err := client.Get("https://ja3er.com/json")
